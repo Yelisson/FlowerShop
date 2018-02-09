6ca68b9db978ea6d524b6b9ba8aac6e104933d91
Elena <Elena@LAPTOP-L7FIB93V>
2018-02-09 18:52:24 +0400

    Обязательно

---
+++ FlowerShopModel/ProductComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShopModel
+{
+    class ProductComponent
+    {
+        public int id { get; set; }
+        public int productId { set; get; }
+        public int componentId { get; set; }
+        public int count { get; set; }
+    }
+}
---
+++ FlowerShopModel/StockComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShopModel
+{
+    class StockComponent
+    {
+        public int id { get; set; }
+        public int stockId { get; set; }
+        public int componentId { get; set; }
+        public int Count { get; set; }
+    }
+}
